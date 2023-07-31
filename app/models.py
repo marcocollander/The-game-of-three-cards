83b45824dd3308577af644c5949263feeba5b46e
Zenon Marek Raubuć <m.raubuc@wp.pl>
2023-08-01 01:29:06 +0200

    Update serializer

--- app/models.py
+++ app/models.py
@@ -1,18 +1,11 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin
 from . import db, login_manager
-
-
-# class Role(db.Model):
-#     __tablename__ = "roles"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(64), unique=True)
-#     users = db.relationship("User", backref="role", lazy="dynamic")
-
-#     def __repr__(self):
-#         return "<Role %r>" % self.name
+import jwt
+import datetime
 
 
 class User(UserMixin, db.Model):
@@ -27,21 +20,36 @@ class User(UserMixin, db.Model):
     def change_results(self, new_results):
         self.results = new_results
         db.session.add(self)
-        db.session.commit();
-
-
+        db.session.commit()
 
     def generate_confirmation_token(self, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'confirm': self.id}).decode('utf-8')
+        #  s = Serializer(current_app.config["SECRET_KEY"], expiration)
+        #  return s.dumps({"confirm": self.id}).decode("utf-8")
+        token = jwt.encode(
+            {
+                "confirm": self.id,
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+                + datetime.timedelta(seconds=expiration),
+            },
+            current_app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+        return token
 
     def confirm(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+        # s = Serializer(current_app.config["SECRET_KEY"])
         try:
-            data = s.loads(token.encode('utf-8'))
+            # data = s.loads(token.encode("utf-8"))
+            data = jwt.decode(
+                token,
+                current_app.config["SECRET_KEY"],
+                leeway=datetime.timedelta(seconds=10),
+                algorithms=["HS256"],
+            )
         except:
             return False
-        if data.get('confirm') != self.id:
+        if data.get("confirm") != self.id:
             return False
         self.confirmed = True
         db.session.add(self)
@@ -49,7 +57,7 @@ class User(UserMixin, db.Model):
 
     @property
     def password(self):
-        raise AttributeError('Nie można odczytać atrybutu hasła')
+        raise AttributeError("Nie można odczytać atrybutu hasła")
 
     @password.setter
     def password(self, password):
@@ -62,17 +70,34 @@ class User(UserMixin, db.Model):
         return "<User %r>" % self.username
 
     def generate_reset_token(self, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'reset': self.id}).decode('utf-8')
+        # s = Serializer(current_app.config["SECRET_KEY"], expiration)
+        # return s.dumps({"reset": self.id}).decode("utf-8")
+        token = jwt.encode(
+            {
+                "confirm": self.id,
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+                + datetime.timedelta(seconds=expiration),
+            },
+            current_app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+        return token
 
     @staticmethod
     def reset_password(token, new_password):
-        s = Serializer(current_app.config['SECRET_KEY'])
+        # s = Serializer(current_app.config["SECRET_KEY"])
         try:
-            data = s.loads(token.encode('utf-8'))
+            # data = s.loads(token.encode("utf-8"))
+            data = jwt.decode(
+                token,
+                current_app.config["SECRET_KEY"],
+                leeway=datetime.timedelta(seconds=10),
+                algorithms=["HS256"],
+            )
         except:
             return False
-        user = User.query.get(data.get('reset'))
+        user = User.query.get(data.get("reset"))
         if user is None:
             return False
         user.password = new_password
